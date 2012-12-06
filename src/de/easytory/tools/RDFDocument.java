b1ae3652ed2456b96025bca68193ba02ab7dce64
ralf <ralf@konwalinka.de>
2012-12-06 17:03:38 +0000

    V1.04: XML-Tool-Class

---
+++ src/de/easytory/tools/RDFDocument.java
@@ -0,0 +1,12 @@
+package de.easytory.tools;
+
+import javax.xml.parsers.ParserConfigurationException;
+
+public class RDFDocument extends XMLDocument 
+{
+	public RDFDocument() throws ParserConfigurationException 
+	{
+		super("rdf:RDF","http://www.w3.org/1999/02/22-rdf-syntax-ns#");
+	}
+
+}
